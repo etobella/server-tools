2a1b1e21c44f05d49f6ce87496a3270a5ad9fa77
Enric Tobella <etobella@creublanca.es>
2018-12-26 22:26:19 +0100

    fixup! [IMP] Tests

--- profiler/tests/test_profiling.py
+++ profiler/tests/test_profiling.py
@@ -1,3 +1,4 @@
+
 # Copyright 2018 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo.tests.common import TransactionCase
@@ -9,8 +10,31 @@ class TestProfiling(TransactionCase):
         """We are testing the creation of a profile."""
         prof_obj = self.env['profiler.profile']
         profile = prof_obj.create({'name': 'this_profiler'})
+        self.assertEqual(0, profiler.attachment_count)
         profile.enable()
+        self.env['res.partner'].search([], limit=1)
         profile.disable()
+        self.assertNotEqual(0, profiler.attachment_count)
+
+    def test_profile_creation(self):
+        """We are testing the creation of a profile."""
+        prof_obj = self.env['profiler.profile']
+        profile = prof_obj.create({
+            'name': 'this_profiler'
+            'use_py_index': True,
+        })
+        self.assertEqual(0, profiler.attachment_count)
+        profile.enable()
+        self.env['res.partner'].search([], limit=1)
+        profile.disable()
+        self.assertNotEqual(0, profiler.attachment_count)
 
     def test_onchange(self):
-        profile = prof_obj.new(
+        profile = prof_obj.create({'name': 'this_profiler'})
+        self.assertFalse(profile.description)
+        profiler.enable_postgresql = True
+        profiler.onchange_enable_postgresql()
+        self.assertTrue(profiler.description)
+        profile.enable()
+        self.env['res.partner'].search([], limit=1)
+        profiler.disable()
