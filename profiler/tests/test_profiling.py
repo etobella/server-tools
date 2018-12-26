94258622f50daa45738f0930f416b1390e1ebfce
Enric Tobella <etobella@creublanca.es>
2018-12-26 22:36:05 +0100

    fixup! fixup! fixup! fixup! [IMP] Tests

--- profiler/tests/test_profiling.py
+++ profiler/tests/test_profiling.py
@@ -10,11 +10,11 @@ class TestProfiling(TransactionCase):
         """We are testing the creation of a profile."""
         prof_obj = self.env['profiler.profile']
         profile = prof_obj.create({'name': 'this_profiler'})
-        self.assertEqual(0, profiler.attachment_count)
+        self.assertEqual(0, profile.attachment_count)
         profile.enable()
         self.env['res.partner'].search([], limit=1)
         profile.disable()
-        self.assertNotEqual(0, profiler.attachment_count)
+        self.assertNotEqual(0, profile.attachment_count)
 
     def test_profile_creation(self):
         """We are testing the creation of a profile."""
@@ -23,19 +23,19 @@ class TestProfiling(TransactionCase):
             'name': 'this_profiler',
             'use_py_index': True,
         })
-        self.assertEqual(0, profiler.attachment_count)
+        self.assertEqual(0, profile.attachment_count)
         profile.enable()
         self.env['res.partner'].search([], limit=1)
         profile.disable()
-        self.assertNotEqual(0, profiler.attachment_count)
+        self.assertNotEqual(0, profile.attachment_count)
 
     def test_onchange(self):
         prof_obj = self.env['profiler.profile']
         profile = prof_obj.create({'name': 'this_profiler'})
         self.assertFalse(profile.description)
-        profiler.enable_postgresql = True
-        profiler.onchange_enable_postgresql()
-        self.assertTrue(profiler.description)
+        profile.enable_postgresql = True
+        profile.onchange_enable_postgresql()
+        self.assertTrue(profile.description)
         profile.enable()
         self.env['res.partner'].search([], limit=1)
-        profiler.disable()
+        profile.disable()
